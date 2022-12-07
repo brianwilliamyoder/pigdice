d837a6a0cc09d0ef7b316e89df769987e918ed6a
ambi hidalgo <ambio.pk@gmail.com>
2022-12-06 20:59:07 -0800

    add handleReset, add onWin, add if 1 in handleTotal, add win message, add start game form, add css styles

--- js/scripts.js
+++ js/scripts.js
@@ -20,13 +20,6 @@ function diceRoll(min, max) {
   return Math.floor(intNum);
 }
 
-// function storeRoundTotal() {
-//   let roundTotal = 0;
-//   let diceNumber = diceRoll(1, 7);
-//   roundTotal += diceNumber;
-//   return roundTotal;
-// 
-
 //UI Logic
 
 function addPlayers(event) {
@@ -34,11 +27,13 @@ function addPlayers(event) {
   let gameDiv = document.querySelector(".hidden");
   let formDiv = document.querySelector(".startForm");
   player1Input = document.getElementById("player1Name").value;
-  player2Input = document.getElementById("player2Name").value
+  player2Input = document.getElementById("player2Name").value;
   player1 = new Player(player1Input);
   player2 = new Player(player2Input);
   game.addPlayer(player1);
   game.addPlayer(player2);
+  document.getElementById("player1Header").innerText = game.players[0].name;
+  document.getElementById("player2Header").innerText = game.players[1].name;
   gameDiv.classList.remove("hidden");
   formDiv.classList.add("hidden");
 }
@@ -50,33 +45,76 @@ function handleDiceRoll() {
   p.innerText = null;
   let dice = diceRoll(1, 7);
   p.append(dice);
-  pInt += dice;
-  pTotal.innerText = pInt.toString();
+  if (dice === 1) {
+    switchPlayer();
+    pTotal.innerText = 0
+    document.getElementById("hold").classList.add("hidden");
+  } else { 
+    pInt += dice;
+    pTotal.innerText = pInt.toString();
+    document.getElementById("hold").classList.remove("hidden");
+  
+  }
+}
+ 
+function handleTotal() {
+  let pTotal = document.getElementById("roundTotalP");
+  let pInt = parseInt(pTotal.innerText);
+  let player1total = document.getElementById("player1Total");
+  let player2total = document.getElementById("player2Total");
+  if (game.turn === 0) {
+    game.players[0].total += pInt;
+    switchPlayer();
+    pTotal.innerText = 0;
+    player1total.innerText = game.players[0].total;
+    document.getElementById("hold").classList.add("hidden");
+  } 
+  else if (game.turn === 1) {
+    game.players[1].total += pInt;
+    switchPlayer();
+    pTotal.innerText = 0;
+    player2total.innerText = game.players[1].total;
+    document.getElementById("hold").classList.add("hidden");
+  }
+  onWin();
 }
 
-
-// function handleTotal() {
-//   let pTotal = document.getElementById("roundTotalP");
-//   let player1Total =
-// }
-
-function switchPlayer() { // game.turn += 1 or -= 1
-                          // switchPlayer = 1 or 0
+function switchPlayer() { 
+  let arrow = document.getElementById("indicator")                       
      if (game.turn === 0) {
+      arrow.innerText = "->"
       game.turn = 1
-      console.log(game.turn)
      }                   
      else if (game.turn === 1) {
+      arrow.innerText = "<-"
       game.turn = 0 
-      console.log(game.turn)     
      }
-
-
 }
+function handleReset() {
+  location.reload()
+}
+ function onWin(){
+  let player1 = game.players[0];
+  let player2 = game.players[1];
+  let gameDiv = document.getElementById("game1");
+  let winDiv = document.getElementById("winMessage");
+  
+  if (player1.total >= 100){
+    gameDiv.classList.add("hidden");
+    winDiv.classList.remove("hidden");
+    document.getElementById("playerName").innerText = game.players[0].name; 
+  } else if (player2.total >=100) {
+    gameDiv.classList.add("hidden");
+    winDiv.classList.remove("hidden");
+    document.getElementById("playerName").innerText = game.players[1].name; 
+  }
+
+ }
 
 
 window.addEventListener("load", function() {
   document.querySelector("form#startGame").addEventListener("submit", addPlayers);
   document.querySelector("button#roll").addEventListener("click", handleDiceRoll);
   document.querySelector("button#hold").addEventListener("click", handleTotal);
+  document.querySelector("button#playAgainButton").addEventListener("click", handleReset);
 });
