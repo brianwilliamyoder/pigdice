8563ae841f4d8c1b1842d6b8c9cafa56eccf211e
ambi hidalgo <ambio.pk@gmail.com>
2022-12-06 18:42:16 -0800

    add switch player func

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,6 @@ function Player(name) {
   this.total = 0
 }
 Game.prototype.addPlayer = function (player){
-  //this.players[player] = player;
   this.players.push(player);
 };
 
@@ -21,11 +20,63 @@ function diceRoll(min, max) {
   return Math.floor(intNum);
 }
 
-function storeRoundTotal() {
-  let roundTotal = 0;
-  let diceNumber = diceRoll(1, 7);
-  roundTotal += diceNumber;
-  return roundTotal;
-}
+// function storeRoundTotal() {
+//   let roundTotal = 0;
+//   let diceNumber = diceRoll(1, 7);
+//   roundTotal += diceNumber;
+//   return roundTotal;
+// 
 
 //UI Logic
+
+function addPlayers(event) {
+  event.preventDefault(); 
+  let gameDiv = document.querySelector(".hidden");
+  let formDiv = document.querySelector(".startForm");
+  player1Input = document.getElementById("player1Name").value;
+  player2Input = document.getElementById("player2Name").value
+  player1 = new Player(player1Input);
+  player2 = new Player(player2Input);
+  game.addPlayer(player1);
+  game.addPlayer(player2);
+  gameDiv.classList.remove("hidden");
+  formDiv.classList.add("hidden");
+}
+
+function handleDiceRoll() {
+  let pTotal = document.getElementById("roundTotalP");
+  let pInt = parseInt(pTotal.innerText);
+  let p = document.getElementById("pDice");
+  p.innerText = null;
+  let dice = diceRoll(1, 7);
+  p.append(dice);
+  pInt += dice;
+  pTotal.innerText = pInt.toString();
+}
+
+
+// function handleTotal() {
+//   let pTotal = document.getElementById("roundTotalP");
+//   let player1Total =
+// }
+
+function switchPlayer() { // game.turn += 1 or -= 1
+                          // switchPlayer = 1 or 0
+     if (game.turn === 0) {
+      game.turn = 1
+      console.log(game.turn)
+     }                   
+     else if (game.turn === 1) {
+      game.turn = 0 
+      console.log(game.turn)     
+     }
+
+
+}
+
+
+window.addEventListener("load", function() {
+  document.querySelector("form#startGame").addEventListener("submit", addPlayers);
+  document.querySelector("button#roll").addEventListener("click", handleDiceRoll);
+  document.querySelector("button#hold").addEventListener("click", handleTotal);
+});
